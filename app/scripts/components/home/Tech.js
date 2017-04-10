c3afc0785f84fcc40e9768968cbdca5b874c4764
Tracy Lee <tlee@inxpo.com>
2017-04-09 21:19:14 -0500

    go live

--- app/scripts/components/home/Tech.js
+++ app/scripts/components/home/Tech.js
@@ -3,18 +3,18 @@
 import React from 'react';
 
 const logos = [
-  {img: 'https://cdn.worldvectorlogo.com/logos/react.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg'},
-  {img: 'https://cldup.com/Rg6WLgqccB.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/angular-icon.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/bower.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/html-5.svg'},
-  {img: 'https://ohdoylerules.com/content/images/css3.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'},
-  {img: 'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/jquery.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/bootstrap-4.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/node-sass.svg'},
-  {img: 'https://cdn.worldvectorlogo.com/logos/gulp.svg'}
+  {img: 'https://cdn.worldvectorlogo.com/logos/react.svg', tip: 'React'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg', tip: 'Node.js'},
+  {img: 'https://cldup.com/Rg6WLgqccB.svg', tip: 'npm'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/angular-icon.svg', tip: 'Angular'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/bower.svg', tip: 'Bower'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/html-5.svg', tip: 'HTML5'},
+  {img: 'https://ohdoylerules.com/content/images/css3.svg', tip: 'CSS3'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg', tip: 'JavaScript'},
+  {img: 'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/jquery.svg', tip: 'JQuery'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/bootstrap-4.svg', tip: 'Bootstrap'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/node-sass.svg', tip: 'Sass'},
+  {img: 'https://cdn.worldvectorlogo.com/logos/gulp.svg', tip: 'Gulp'}
 ];
 
 const Tech = React.createClass({
@@ -24,8 +24,13 @@ const Tech = React.createClass({
       logos: []
     };
   },
+
   componentDidMount: function() {
+    $(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
   },
+
   render: function() {
     return (
       <div>
@@ -34,7 +39,9 @@ const Tech = React.createClass({
           {logos.map(function(item, index) {
             return (
               <div className="flex-column-2 flex gutter" key={index}>
-                <img className="tech-icon" src={item.img} />
+                <img className="tech-icon" src={item.img}
+                data-toggle="tooltip"
+                data-placement="top" title={item.tip}/>
               </div>
             );
           })}
